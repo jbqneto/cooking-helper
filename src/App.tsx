363643fb6219adc5ece9fec0485d9888df2761c5
Jose Bezerra <jbqneto@gmail.com>
2023-05-28 16:24:16 +0100

    project sketch

---
+++ src/App.tsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Chip,
+  Divider,
+  IconButton,
+  Stack,
+  Switch,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+import EditNoteOutlinedIcon from "@mui/icons-material/EditNoteOutlined";
+
+type Ingredient = {
+  name: string;
+  qtd: number;
+};
+
+type Input = Omit<Ingredient, ""> & {
+  disabled?: boolean;
+};
+
+function App() {
+  const [isEditing, setIsEditing] = useState(-1);
+  const [oldQtd, setOldQtd] = useState(0);
+  const [inputs, setInputs] = useState<Input[]>([
+    {
+      name: "",
+      qtd: 0,
+    },
+  ]);
+
+  const handleAdd = () => {
+    setInputs([...inputs, { name: "", qtd: 0 }]);
+  };
+
+  const handleProportions = (index: number) => {
+    setOldQtd(inputs[index].qtd);
+    setIsEditing(index);
+  };
+
+  const handleQtdChange = (index: number, event: any) => {
+    const oldQtd = inputs[index].qtd;
+    const newQtd = event?.target.value ?? oldQtd;
+
+    handleFormChange(index, event);
+  };
+
+  const handleFormChange = (index: number, event: any) => {
+    const ingredients = [...inputs];
+    const ingredient = ingredients[index];
+
+    if (ingredient) {
+      ingredient[event.target.name] = event.target.value;
+    }
+
+    setInputs(ingredients);
+  };
+
+  const handleSave = () => {
+    let _inputs = inputs.map((input) => ({
+      ...input,
+      disabled: true,
+    }));
+
+    if (isEditing !== -1) {
+      const proportion = inputs[isEditing].qtd / oldQtd;
+
+      _inputs = _inputs.map((input, index) => {
+        return {
+          ...input,
+          qtd: index === isEditing ? input.qtd : input.qtd * proportion,
+        };
+      });
+
+      setIsEditing(-1);
+      setOldQtd(0);
+    }
+
+    setInputs(_inputs);
+  };
+
+  const getFormGroup = (index: number, input: Input) => {
+    return (
+      <div key={index} className="form-group">
+        <TextField
+          label="Ingrediente"
+          value={input.name}
+          name="name"
+          disabled={input.disabled}
+          variant="standard"
+          onChange={(event) => handleFormChange(index, event)}
+        />
+        <TextField
+          label="Quantidade"
+          value={input.qtd}
+          name="qtd"
+          disabled={input.disabled && isEditing !== index}
+          variant="standard"
+          onChange={(event) => handleQtdChange(index, event)}
+        />
+        <EditNoteOutlinedIcon
+          onClick={() => handleProportions(index)}
+        ></EditNoteOutlinedIcon>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <main>
+        <div className="title">
+          <h2>Receitas</h2>
+        </div>
+        <div className="actions">
+          <Button onClick={(event) => handleAdd()} variant="contained">
+            Adicionar ingrediente
+          </Button>
+        </div>
+        <div className="content">
+          <form action="">
+            {inputs.map((ingredient, index) => getFormGroup(index, ingredient))}
+            <hr />
+            <div className="btns">
+              <Button onClick={(event) => handleSave()} variant="contained">
+                Salvar
+              </Button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
